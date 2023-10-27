04cb01d0d26998c545ba4f37ff79ae8fe043db30
AlexReis1313 <alexandreleitereis@gmail.com>
2023-10-27 16:12:00 +0100

    Scripts terminados

--- Task14.m
+++ Task14.m
@@ -1,21 +1,21 @@
 %% Task 14 script
-clc
-load('measurements.mat', 'xgt', 'vgt', 'tref', 'a', 'r', 'u', 'v', 'rr');
+clc, clear
+load('measurements.mat', 'xgt', 'vgt', 'tref', 'a', 'r', 'u', 'v', 'rr'); % 'mh370.mat' can also be used instead of 'measurements.mat'
 
 %% Problem formulation
-figure
-%still in progress - a ser feito por Alex
-%subplot(2,1,1); 
-x0=(xgt(1,:))';
+x0=(xgt(1,:))';         % fixed value for x_0
 v_x = linspace(-5,5);
 v_y = linspace(-2.5,7.5);
 [X,Y] = meshgrid(v_x,v_y);
-
+niu=1;
 for i=1:length(v_y)
     for j=1:length(v_x)
-        vvalues(i,j) = cost_function([v_x(j);v_y(i)], a, x0,tref, r, rr);
+        vvalues(i,j) = cost_function([v_x(j);v_y(i)], a, x0,tref, r, rr, niu); %cost function's value for each possible velocity vector
      end
 end
+
+%% Plots
+figure
 contour(v_x,v_y, vvalues,50, 'ShowText','off');
 hold on 
 xline(0, 'Color', 'k', 'LineWidth', 0.5);
@@ -26,58 +26,27 @@ ylabel('\textbf{Velocity y}', 'Interpreter','latex') ;
 scatter3(v(1,1),v(1,2),0,'r','filled')
 text(v(1,1),v(1,2)+0.5,'\textbf{True $v$}','Interpreter','latex')
 
-% subplot(2,1,2); 
-% x0_x = linspace(-25,25);
-% x0_y = linspace(-25,25);
-% [X,Y] = meshgrid(x0_x,x0_y);
-% 
-% for i=1:length(x0_y)
-%     for j=1:length(x0_x)
-%         xvalues(i,j) = cost_function(v(1,:), a, [x0_x(j);x0_y(i)],tref, r, rr);
-%      end
-% end
-% contour(x0_x,x0_y, xvalues,40, 'ShowText','off');
-% hold on 
-% xline(0, 'Color', 'k', 'LineWidth', 0.5);
-% yline(0, 'Color', 'k', 'LineWidth', 0.5);
-% title('\textbf{Cost Function contour lines, as a function of the inicial position $x_0$ (fixed to true value of the velocity)}', 'Interpreter','latex')
-% xlabel('\textbf{Inicial position x-axis}','Interpreter','latex');
-% ylabel('\textbf{Inicial position y-axis}', 'Interpreter','latex') ;
-% ylim([-25 25])
-% xlim([-25 25])
-% 
-% scatter3(x0(1),x0(2),0,'r','filled')
-% text(x0(1),x0(2)+1,'\textbf{True $x_0$}','Interpreter','latex')
 
 figure %3d plot
-s=surf(v_x,v_y, vvalues)
+s=surf(v_x,v_y, vvalues);
 s.EdgeColor = 'none';
 hold on
-scatter3(v(1,1),v(1,2),cost_function([v(1,1);v(1,2)], a, x0,tref, r, rr),'r','filled')
+scatter3(v(1,1),v(1,2),cost_function([v(1,1);v(1,2)], a, x0,tref, r, rr, niu),'r','filled')
 legend('\textbf{Cost Function, as a function of the 2D velocity vector ($x_0$ fixed to true value)}','True $v$', 'Interpreter','latex')
 hold off
 
-% figure %3d plot
-% s2=surf(x0_x,x0_y, xvalues)
-% s2.EdgeColor = 'none';
-% legend('\textbf{Cost Function contour lines, as a function of the inicial position $x_0$ (fixed to true value of the velocity)}', 'Interpreter','latex')
-% hold on
-% scatter3(x0(1),x0(2),0,'r','filled')
-% text(x0(1),x0(2)+1,'\textbf{True $x_0$}','Interpreter','latex')
-% hold off
-
-function F = cost_function(v, a, x0 ,tref, r, rr)
-    delta=tref(2) - tref(1);
-    F=0;
-    derivative_term=0;
-    for i = 1:length(tref) 
-        if i == 1
-            derivative_term = ( norm(x0+v*tref(i+1)-a') - norm(x0+v*tref(i)-a') )/(delta);
-        elseif i == length(tref) 
-            derivative_term = ( norm(x0+v*tref(i)-a') - norm(x0+v*tref(i-1)-a') )/(delta);
-        else
-            derivative_term = ( norm(x0+v*tref(i+1)-a') - norm(x0+v*tref(i-1)-a') )/(2*delta);
-        end
-    F = F + (norm(x0+v*tref(i)-a') - r(i)).^2 + norm(v)*((derivative_term - rr(i)).^2);
+%% Functions to support problem formulation
+function residual = residual_func(v, a, x0, tref, r, rr, niu)
+    for i = 1:length(tref)
+        aux=x0+v*tref(i)-a';
+        rR(i)=norm(aux) - r(i);
+        rRR(i)= sqrt(niu)*( (v'*(aux)/(norm(aux))) - rr(i));
     end
+    residual=[rR,rRR]';
+end
+function F = cost_function(v, a, x0 ,tref, r, rr, niu)
+  
+    residual=residual_func(v, a, x0, tref, r, rr, niu);
+    F= 0.5*(norm(residual))^2;    
+
 end
--- Task16.m
+++ Task16.m
@@ -1,49 +1,43 @@
 %% Task 16 script
 clear
 clc
-load('mh370.mat', 'xgt', 'vgt', 'tref', 'a', 'r', 'u', 'v', 'rr');
+load('measurements.mat', 'xgt', 'vgt', 'tref', 'a', 'r', 'u', 'v', 'rr'); % 'mh370.mat' can also be used instead of 'measurements.mat'
 %% Problem formulation
-% Set initial values for vel and x0
 
-x0=(xgt(1,:))';
-niu=1;
-E=1e-6;
-v_true=v(1,:);
-k=1; %k is inicialized at 1, because matlab arrays' first index is 1
-vel(:,k) = [-3;3];
-lambda(k)=1;
-validation=0;
-rejection=0;
+vel(:,1) = [3;3];   % Initial v prediction
 
+x0=(xgt(1,:))';     % Set initial values for x0
+niu=1;              % Defining cost function's hyperparameter
+E=1e-6;             % Stop condition - for cost function gradient's norm
+v_true=v(1,:);      % True velocity value
+k=1;                % k is inicialized at 1, because matlab arrays' first index is 1
+lambda(k)=1;        % Initial lambda - penalizes deviations of v_hat(k+1) from v(k) in the LM Method
 
-while 1
-    J(:,:,k)= residual_Jacbobian(vel(:,k), a, x0, tref, niu);
-    residual(:,k)=residual_func(vel(:,k), a, x0, tref, r, rr, niu);
-    fval(k)=cost_function(vel(:,k),a, x0 ,tref, r, rr, niu);
+while 1 %while true in matlab
+    J(:,:,k)= residual_Jacbobian(vel(:,k), a, x0, tref, niu);       %residual for vel(k)
+    residual(:,k)=residual_func(vel(:,k), a, x0, tref, r, rr, niu); %jacobian for vel(K)
+    fval(k)=cost_function(vel(:,k),a, x0 ,tref, r, rr, niu); %cost function's value forvel(k)
     g(k,:)= (J(:,:,k))'*residual(:,k); %gradient of f(v)
-    g_norm(k)=norm(g(k,:));
+    g_norm(k)=norm(g(k,:));    
 
     if g_norm(k)<E | k>1500 %function converged to local minima - output results
-        output_func(fval, g_norm, vel, v_true)
+        output_func(fval, g_norm, vel, v_true, k-1, niu)
         break
     end
    
-    v_hat(:,k)= argmin_function(lambda(k), vel(:,k),residual(:,k),J(:,:,k),g(k,:));
+    v_hat(:,k)= argmin_function(lambda(k), vel(:,k),residual(:,k),J(:,:,k),g(k,:)); %standard least squares problem 
     
     if cost_function(v_hat(:,k),a, x0 ,tref, r, rr, niu)<fval(k)%valid step
         vel(:,k+1)=v_hat(:,k);
         lambda(k+1)=0.7*lambda(k) ;
-        validation=validation+1;
     else %null step
         vel(:,k+1)=vel(:,k);
         lambda(k+1)=2*lambda(k);
-        rejection=rejection+1;
     end
     k=k+1;
-    end
-
+end
+    
 %% Functions to support optimization
-
 function residual = residual_func(v, a, x0, tref, r, rr, niu)
     for i = 1:length(tref)
         aux=x0+v*tref(i)-a';
@@ -52,14 +46,11 @@ function residual = residual_func(v, a, x0, tref, r, rr, niu)
     end
     residual=[rR,rRR]';
 end
-
-
 function J= residual_Jacbobian(v, a, x0, tref, niu)
     for i = 1:length(tref)
         %for range
         aux=x0+v*tref(i)-a';
         jR(i,:) = ((aux/norm(aux)) *tref(i))';
-        
         %for range rate
         P = v'*aux;
         Q = norm(aux);
@@ -69,7 +60,6 @@ function J= residual_Jacbobian(v, a, x0, tref, niu)
     end
 J=[jR;jRR];
 end
-
 function v_minima= argmin_function(lambda, vel, residual,J,g)
     T=size(J,1)/2;
     %aqui não tamos a transpor a jacobiana e na teoria deviamos
@@ -82,21 +72,20 @@ function v_minima= argmin_function(lambda, vel, residual,J,g)
     A=[ grad_f1 ; grad_f2; sqrt(lambda)*eye(2)]; %matrix identidade 2x2 porque multiplica por vel (2x1)
     b=[b1 ;b2; sqrt(lambda)*vel];
     v_minima = lsqlin(A, b);
-
 end
 
-function output_func(cost_fun, gradient, velocity, v_true)
+function output_func(cost_fun, gradient, velocity, v_true, k, niu)
     figure      
     subplot(2,1,1)
     title('\textbf{Optimaztion parameters - Levenberg-Marquard method}', 'Interpreter','latex')
     yyaxis left
     plot(gradient)
-    ylabel("\textbf{Norm of the residuals' gradient}", 'Interpreter','latex')
+    ylabel("\textbf{$\| \nabla f(v_k) \|$}", 'Interpreter','latex')
     %ylim([0 100])
     yyaxis right
     plot(cost_fun)
-    ylabel("\textbf{Cost function}", 'Interpreter','latex')
-    legend("\textbf{Norm of the residual's gradient $\| \nabla f(v_k) \|$}",'\textbf{Cost function: $\sum_{t \in T} (\hat{r}(t) - r_t)^2 + \nu(\hat{s}(t) - s_t)^2$}', 'Interpreter','latex')
+    ylabel("\textbf{$f(v)$}", 'Interpreter','latex')
+    legend("\textbf{Norm of the cost-function's gradient $\| \nabla f(v_k) \|$}",'\textbf{Cost-function: $f(v)$}', 'Interpreter','latex')
     xlabel('\textbf{Iterations}', 'Interpreter','latex')
     %ylim([0 100])
     subplot(2,1,2)
@@ -110,12 +99,13 @@ function output_func(cost_fun, gradient, velocity, v_true)
     title('Velocity', 'Interpreter','latex');
     hold off
     
-    print
-
+   fprintf('Final gradient norm: %e\n', gradient(end))
+   fprintf('Final cost-function value (fval): %e\n', cost_fun(end))
+   fprintf('Number of iterations: %d\n', k)
+   %fprintf('Hyperparameter niu %d\n', niu)
+   fprintf('Predicted velocity: [ %f , %f ]\n', velocity(1,end),velocity(2,end))
 end
 function F = cost_function(v, a, x0 ,tref, r, rr, niu)
-  
-residual=residual_func(v, a, x0, tref, r, rr, niu);
-F= 0.5*(norm(residual))^2;    
-
+    residual=residual_func(v, a, x0, tref, r, rr, niu);
+    F= 0.5*(norm(residual))^2;    
 end
